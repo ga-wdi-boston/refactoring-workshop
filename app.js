f81c97792a056259b7d11065468852ebab7a7409
Tom Dyer <tdyer1@gmail.com>
2015-10-21 14:27:42 -0400

    Added Login

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
   // Registration Handler/Callback
   $('#register').on('submit', User.registrationHandler);
 
+  // User Login Handler/Callback
+  $('#login').on('submit', User.loginHandler);
+
 });
--- user.js
+++ user.js
@@ -1,4 +1,46 @@
-var User = function(){
+var User = function(email, id, token){
+  this.email = email;
+  this.id = id;
+  this.token = token;
+};
+
+
+User.loginHandler = function(event){
+  event.preventDefault();
+  var $form = $('#login');
+  var $email = $form.find("[name='email']");
+  var $pw = $form.find("[name='password']");
+
+  var cred = {
+    "credentials": {
+      "email": $email.val(),
+      "password": $pw.val()
+    }
+  };
+
+  $.ajax({
+    method: 'POST',
+    // TODO: remove hardcoded URL
+    url: 'http://localhost:3000' + '/login',
+    contentType: 'application/json',
+    data: JSON.stringify(cred),
+    dataType: 'json'
+  })
+    .done(function(data){
+      console.log('login data is ', data);
+      User.current_user = new User(data.user.email, data.user.id, data.user.id);
+
+      Util.display('Logged In User '+ data.user.email);
+      Util.showResult(data);
+
+      $email.val('');
+      $pw.val('');
+
+    })
+    .fail(function(){
+      console.error('Failed to login user ' + $email.val());
+      Util.display('Failed to Login User '+ $email.val());
+    });
 };
 
 User.registrationHandler = function(event){
